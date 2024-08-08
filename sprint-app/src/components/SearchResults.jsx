3fd364f84984d7f007c4e0a55e43ab743da06175
Austin <Austin.reid@keyin.com>
2024-08-08 19:43:20 -0230

    Fixed Formatting

--- sprint-app/src/components/SearchResults.jsx
+++ sprint-app/src/components/SearchResults.jsx
@@ -1,4 +1,19 @@
-<p>{results.message}</p>
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from './partials/Head';
+import Nav from './partials/Nav';
+import Foot from './partials/Foot';
+
+const SearchResults = ({ results }) => {
+    return (
+        <div>
+            <Head />
+            <Nav />
+            <div>
+                <h1>Search Results</h1>
+                {results ? (
+                    results.message ? (
+                        <p>{results.message}</p>
                     ) : (
                         <>
                             <h2>Name: {results.name || 'N/A'}</h2>
@@ -48,4 +63,30 @@
                                 </div>
                             )}
                         </>
-                    )
+                    )                    
+                ) : (
+                    <p>No search results found.</p>
+                )}
+            </div>
+            <Foot />
+        </div>
+    );
+};
+
+SearchResults.propTypes = {
+    results: PropTypes.shape({
+        message: PropTypes.string,
+        name: PropTypes.string,
+        birthday: PropTypes.string,
+        gifts: PropTypes.objectOf(
+            PropTypes.arrayOf(PropTypes.string)
+        ),
+        likes: PropTypes.arrayOf(PropTypes.string),
+        dislikes: PropTypes.arrayOf(PropTypes.string),
+        loves: PropTypes.arrayOf(PropTypes.string)
+    })
+};
+
+SearchResults.defaultProps = {};
+
+export default SearchResults;
