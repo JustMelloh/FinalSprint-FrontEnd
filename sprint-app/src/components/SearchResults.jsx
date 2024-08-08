54c02b06956e0438071f29d69a45f92c0fc407ec
Austin <Austin.reid@keyin.com>
2024-08-08 19:42:21 -0230

    Fixed so Objects become Strings

--- sprint-app/src/components/SearchResults.jsx
+++ sprint-app/src/components/SearchResults.jsx
@@ -1,19 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from './partials/Head';
-import Nav from './partials/Nav';
-import Foot from './partials/Foot';
-
-const SearchResults = ({ results }) => {
-    return (
-        <div>
-            <Head />
-            <Nav />
-            <div>
-                <h1>Search Results</h1>
-                {results ? (
-                    results.message ? (
-                        <p>{results.message}</p>
+<p>{results.message}</p>
                     ) : (
                         <>
                             <h2>Name: {results.name || 'N/A'}</h2>
@@ -24,7 +9,7 @@ const SearchResults = ({ results }) => {
                                         <h3>{category.toUpperCase()}</h3>
                                         <ul>
                                             {results.gifts[category].map((gift, index) => (
-                                                <li key={index}>{gift}</li>
+                                                <li key={index}>{typeof gift === 'object' ? JSON.stringify(gift) : gift}</li>
                                             ))}
                                         </ul>
                                     </div>
@@ -37,7 +22,7 @@ const SearchResults = ({ results }) => {
                                     <h3>Likes</h3>
                                     <ul>
                                         {results.likes.map((like, index) => (
-                                            <li key={index}>{like}</li>
+                                            <li key={index}>{typeof like === 'object' ? JSON.stringify(like) : like}</li>
                                         ))}
                                     </ul>
                                 </div>
@@ -47,7 +32,7 @@ const SearchResults = ({ results }) => {
                                     <h3>Dislikes</h3>
                                     <ul>
                                         {results.dislikes.map((dislike, index) => (
-                                            <li key={index}>{dislike}</li>
+                                            <li key={index}>{typeof dislike === 'object' ? JSON.stringify(dislike) : dislike}</li>
                                         ))}
                                     </ul>
                                 </div>
@@ -57,36 +42,10 @@ const SearchResults = ({ results }) => {
                                     <h3>Loves</h3>
                                     <ul>
                                         {results.loves.map((love, index) => (
-                                            <li key={index}>{love}</li>
+                                            <li key={index}>{typeof love === 'object' ? JSON.stringify(love) : love}</li>
                                         ))}
                                     </ul>
                                 </div>
                             )}
                         </>
                     )
-                ) : (
-                    <p>No search results found.</p>
-                )}
-            </div>
-            <Foot />
-        </div>
-    );
-};
-
-SearchResults.propTypes = {
-    results: PropTypes.shape({
-        message: PropTypes.string,
-        name: PropTypes.string,
-        birthday: PropTypes.string,
-        gifts: PropTypes.objectOf(
-            PropTypes.arrayOf(PropTypes.string)
-        ),
-        likes: PropTypes.arrayOf(PropTypes.string),
-        dislikes: PropTypes.arrayOf(PropTypes.string),
-        loves: PropTypes.arrayOf(PropTypes.string)
-    })
-};
-
-SearchResults.defaultProps = {};
-
-export default SearchResults;
